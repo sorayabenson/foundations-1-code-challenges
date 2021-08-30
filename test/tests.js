122ca7473e25559c2ed88da72383ea5df2c4f90c
Soraya Benson <soraya.benson@gmail.com>
2021-08-30 14:55:49 -0700

    forEach tests pass

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
-// import './for-each.test.js';
+import './for-each.test.js';
 // import './map.test.js';
-import './filter-find.test.js';
+// import './filter-find.test.js';
 // import './object-iteration.test.js';
 // import './stretch.test.js';
